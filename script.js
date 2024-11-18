ebc9a030b8a9a7bd2a5b8e64adb05d1f6749d555
DarnellCodealot <cfghxftdgugxugg@gmail.com>
2024-11-18 06:46:16 +0200

    Better text

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-window.onload = aboutPageInfo
+window.onload = onpageload
+
+
+function onpageload(){
+    aboutPageInfo().then(() => {interactiveText()}).catch(() => {interactiveText()});
+
+}
 
 function displaceContact(){
     let cb = document.getElementById("contact-button");
@@ -7,6 +13,8 @@ function displaceContact(){
 }
 
 function aboutPageInfo(){
+    return new Promise((resolve, reject) =>
+    {
     let profpic = document.getElementById("profile-pic")
     let proftext = document.getElementById("profile-text")
     fetch("https://random-person-generator.com/api?gender=m")
@@ -18,12 +26,34 @@ function aboutPageInfo(){
   })
   .then(data => {
     console.log(data); // Handle the data from the API
-    profpic.src = data["profile_photo"]["256x256"]
+    profpic.src = data["profile_photo"]["256x256"];
     proftext.innerText = "My name is " + data["identification"]["full_name"] + ", and I am a " + data["identification"]["age"] + "-year-old " + data["identification"]["nationality"] + " " + data["employment"]["occupation"] + " working in the " + data["employment"]["industry"] + " industry. I am currently employed by " + data["employment"]["employer"] + ", where I contribute to exciting projects and collaborate with talented individuals to achieve impactful results. This website is a personal project that I developed in my free time as a way to further explore my skills and share my work with a broader audience. With a strong passion for innovation and problem-solving, I strive to stay ahead in the ever-evolving " + data["employment"]["industry"] + " landscape.";
+    resolve();
   })
   .catch(error => {
     console.error('There was a problem with the fetch operation:', error);
+    reject();
   });
+}, 1000);
+}
 
-
+function interactiveText(){
+    let hs = document.querySelectorAll(".hover-element");
+    console.log(hs);
+    for(let i = 0; i < hs.length; i++){
+        console.log(hs[i]);
+        let l = true;
+        for(let j = 0; j < hs[i].innerHTML.length; j++){
+            if(l && hs[i].innerHTML[j] != " "){
+                hs[i].innerHTML = hs[i].innerHTML.slice(0,j) + "<span class=\"hover-text\">" + hs[i].innerHTML.slice(j);
+                l = false;
+                j = j+"<span class=\"hover-text\">".length;
+            }
+            else if(!l && hs[i].innerHTML[j] == " "){
+                hs[i].innerHTML = hs[i].innerHTML.slice(0,j) + "</span>" + hs[i].innerHTML.slice(j);
+                l = true;
+                j = j+"</span>".length;
+            }
+        }
+    }
 }
